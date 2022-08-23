8a243296cc21f0f668be7490d2f11f4ea48693ab
Amaurysj <you@example.comamaurysj@gmail.com>
2022-08-23 13:02:45 -0300

    first commit

---
+++ palavras/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Usando_var
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string[] palavras = { "Macoratti", "Miriam", "Jeferson", "Janice", "Mario"};
+            var consulta = from palavra in palavras
+                           where palavra[2] == 'r'
+                           select palavra;
+            foreach ( string p in consulta)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}
